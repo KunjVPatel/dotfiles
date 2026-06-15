7a4b9a3cd51408edc66c09337ff9d7d969bc1aef
Kunj <kunjvpatel321@gmail.com>
2026-06-15 02:23:59 -0400

    update

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,199 +1,246 @@
--- Set leader key to Space
+-- =========================
+-- BASIC SETTINGS
+-- =========================
 vim.g.mapleader = " "
-
--- Enable true colors and theme
 vim.o.termguicolors = true
 vim.o.background = "dark"
-
--- Line numbers & indentation
 vim.o.number = true
 vim.o.relativenumber = true
 vim.o.tabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
-
--- Searching
 vim.o.ignorecase = true
 vim.o.smartcase = true
-
--- Mouse support
 vim.o.mouse = "a"
 
--- Keybindings
-vim.api.nvim_set_keymap("n", "<leader>w", ":w<CR>", { noremap = true, silent = true }) -- Save
-vim.api.nvim_set_keymap("n", "<leader>q", ":q<CR>", { noremap = true, silent = true }) -- Quit
+-- =========================
+-- KEYMAPS
+-- =========================
+local map = vim.keymap.set
+map("n", "<leader>w", ":w<CR>")
+map("n", "<leader>q", ":q<CR>")
+map("n", "<C-h>", "<C-w>h")
+map("n", "<C-j>", "<C-w>j")
+map("n", "<C-k>", "<C-w>k")
+map("n", "<C-l>", "<C-w>l")
 
--- Window navigation
-vim.api.nvim_set_keymap("n", "<C-h>", "<C-w>h", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-j>", "<C-w>j", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-k>", "<C-w>k", { noremap = true })
-vim.api.nvim_set_keymap("n", "<C-l>", "<C-w>l", { noremap = true })
+-- RMarkdown knit to PDF
+map("n", "<leader>rr", function()
+    local file = vim.fn.expand("%:p")
+    vim.cmd("!" .. "Rscript -e \"rmarkdown::render('" .. file .. "', output_format='pdf_document')\"")
+end)
 
--- Terminal (Toggle with Shift+`)
-vim.api.nvim_set_keymap("n", "<S-`>", ":split | terminal<CR>", { noremap = true, silent = true })
+-- Open PDF after render
+map("n", "<leader>rv", function()
+    local pdf = vim.fn.expand("%:p:r") .. ".pdf"
+    vim.cmd("vsplit | terminal pdftotext " .. pdf .. " - | less")
+end)
 
--- Bootstrap lazy.nvim if not installed
+-- =========================
+-- LAZY BOOTSTRAP
+-- =========================
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
     vim.fn.system({
         "git", "clone", "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git", lazypath,
+        "https://github.com/folke/lazy.nvim.git",
+        lazypath,
     })
-    end
-    vim.opt.rtp:prepend(lazypath) -- Ensure lazy.nvim actually gets loaded
+end
+vim.opt.rtp:prepend(lazypath)
 
-    -- Install and configure plugins
-    require("lazy").setup({
-        -- File Explorer (Sidebar)
-    {
-        "nvim-tree/nvim-tree.lua",
-        config = function()
-        require("nvim-tree").setup()
-        vim.api.nvim_set_keymap("n", "<leader>e", ":NvimTreeToggle<CR>", { noremap = true, silent = true })
-        end,
-    },
+-- =========================
+-- PLUGINS
+-- =========================
+require("lazy").setup({
 
-    -- Gruvbox Theme
+    rocks = { enabled = false },
+
+    -- THEME
     {
         "morhetz/gruvbox",
+        priority = 1000,
         config = function()
-        vim.cmd("colorscheme gruvbox")
+            vim.cmd("colorscheme gruvbox")
         end,
     },
 
-    -- Status Line
+    -- FILE TREE
+    {
+        "nvim-tree/nvim-tree.lua",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
+        config = function()
+            require("nvim-tree").setup()
+            map("n", "<leader>e", ":NvimTreeToggle<CR>")
+        end,
+    },
+
+    -- STATUS LINE
     {
         "nvim-lualine/lualine.nvim",
         dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-        require("lualine").setup()
+            require("lualine").setup()
         end,
     },
 
-    -- Tabs / Bufferline
+    -- BUFFERLINE
     {
         "akinsho/bufferline.nvim",
-        dependencies = "nvim-tree/nvim-web-devicons",
+        dependencies = { "nvim-tree/nvim-web-devicons" },
         config = function()
-        require("bufferline").setup()
-        vim.api.nvim_set_keymap("n", "<Tab>", ":BufferLineCycleNext<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<S-Tab>", ":BufferLineCyclePrev<CR>", { noremap = true, silent = true })
+            require("bufferline").setup()
+            map("n", "<Tab>",   ":BufferLineCycleNext<CR>")
+            map("n", "<S-Tab>", ":BufferLineCyclePrev<CR>")
         end,
     },
 
-    -- Syntax Highlighting (Treesitter)
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        config = function()
-        require("nvim-treesitter.configs").setup {
-            ensure_installed = { "python", "cpp", "r", "lua", "bash", "sql", "latex", "rust" },
-            highlight = { enable = true },
-        }
-        end,
-    },
-
-    -- Terminal (ToggleTerm for bottom terminal)
-    {
-        "akinsho/toggleterm.nvim",
-        config = function()
-        require("toggleterm").setup({
-            open_mapping = [[<leader>`]], -- Space + ` to toggle the terminal
-            direction = "horizontal",
-            size = function(term)
-            return vim.o.lines * 0.25  -- Set terminal height to 1/4th of the screen
-            end,
-            shade_terminals = true,
-            shading_factor = 2,
-            start_in_insert = true,
-            persist_size = true,
-        })
-        end,
-    },
-
-    -- Fuzzy Finder (Telescope)
+    -- =========================
+    -- TELESCOPE
+    -- =========================
     {
         "nvim-telescope/telescope.nvim",
-        dependencies = { "nvim-lua/plenary.nvim" },
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            { "nvim-telescope/telescope-fzf-native.nvim", build = "make" },
+        },
         config = function()
-        local builtin = require("telescope.builtin")
-        vim.api.nvim_set_keymap("n", "<leader>ff", ":Telescope find_files<CR>", { noremap = true, silent = true })
+            local telescope = require("telescope")
+            telescope.setup({})
+            telescope.load_extension("fzf")
+            map("n", "<leader>ff", "<cmd>Telescope find_files<cr>")
+            map("n", "<leader>fg", "<cmd>Telescope live_grep<cr>")
+            map("n", "<leader>fb", "<cmd>Telescope buffers<cr>")
         end,
     },
 
-    -- Language Server Protocol (LSP) & Auto-completion
+    -- =========================
+    -- LSP + MASON
+    -- =========================
     {
-        "neovim/nvim-lspconfig",
+        "williamboman/mason.nvim",
         config = function()
-        require("lspconfig").pyright.setup{} -- Python LSP
-        require("lspconfig").clangd.setup{}  -- C++ LSP
-        require("lspconfig").r_language_server.setup{}  -- R LSP
+            require("mason").setup()
         end,
     },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        dependencies = {
+            "williamboman/mason.nvim",
+            "neovim/nvim-lspconfig",
+        },
+        config = function()
+            require("mason-lspconfig").setup({
+                ensure_installed = { "pyright", "clangd", "r_language_server" },
+                handlers = {
+                    function(server_name)
+                        require("lspconfig")[server_name].setup({})
+                    end,
+                },
+            })
+        end,
+    },
+
+    -- =========================
+    -- AUTOCOMPLETE
+    -- =========================
     {
         "hrsh7th/nvim-cmp",
         dependencies = {
             "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path",
             "L3MON4D3/LuaSnip",
         },
         config = function()
-        local cmp = require("cmp")
-        cmp.setup({
-            mapping = cmp.mapping.preset.insert({
-                ["<Tab>"] = cmp.mapping.select_next_item(),
-                                                ["<S-Tab>"] = cmp.mapping.select_prev_item(),
-                                                ["<CR>"] = cmp.mapping.confirm({ select = true }),
-            }),
-            sources = cmp.config.sources({
-                { name = "nvim_lsp" },
-                { name = "buffer" },
-                { name = "path" },
-            }),
-        })
+            local cmp = require("cmp")
+            cmp.setup({
+                mapping = cmp.mapping.preset.insert({
+                    ["<Tab>"]   = cmp.mapping.select_next_item(),
+                    ["<S-Tab>"] = cmp.mapping.select_prev_item(),
+                    ["<CR>"]    = cmp.mapping.confirm({ select = true }),
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                }),
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "buffer" },
+                    { name = "path" },
+                },
+            })
         end,
     },
 
-    -- Git Integration
-    {
-        "tpope/vim-fugitive",
-        config = function()
-        vim.api.nvim_set_keymap("n", "<leader>gs", ":Git<CR>", { noremap = true, silent = true })
-        end,
-    },
-
-    -- LaTeX Support
-    {
-        "lervag/vimtex",
-        config = function()
-        vim.g.vimtex_view_method = "zathura"
-        end,
-    },
-
-    -- Jupyter Notebook Execution (Magma)
+    -- =========================
+    -- DATA SCIENCE TOOLS
+    -- =========================
     {
         "dccsillag/magma-nvim",
         build = ":UpdateRemotePlugins",
         config = function()
-        vim.g.magma_automatically_open_output = false
-        vim.api.nvim_set_keymap("n", "<leader>r", ":MagmaEvaluateLine<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("v", "<leader>r", ":<C-u>MagmaEvaluateVisual<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<leader>o", ":MagmaShowOutput<CR>", { noremap = true, silent = true })
+            vim.g.magma_automatically_open_output = false
+            map("n", "<leader>r", ":MagmaEvaluateLine<CR>")
+            map("v", "<leader>r", ":<C-u>MagmaEvaluateVisual<CR>")
+            map("n", "<leader>o", ":MagmaShowOutput<CR>")
+        end,
+    },
+    {
+        "cameron-wags/rainbow_csv.nvim",
+        config = true,
+    },
+
+    -- =========================
+    -- GIT
+    -- =========================
+    {
+        "tpope/vim-fugitive",
+        config = function()
+            map("n", "<leader>gs", ":Git<CR>")
         end,
     },
 
-    -- Debugging (nvim-dap)
+    -- =========================
+    -- TERMINAL
+    -- =========================
+    {
+        "akinsho/toggleterm.nvim",
+        config = function()
+            require("toggleterm").setup({
+                open_mapping    = [[<leader>`]],
+                direction       = "horizontal",
+                size            = function() return vim.o.lines * 0.25 end,
+                shade_terminals = true,
+                shading_factor  = 2,
+                start_in_insert = true,
+                persist_size    = true,
+            })
+        end,
+    },
+
+    -- =========================
+    -- DEBUG (DAP)
+    -- =========================
     {
         "mfussenegger/nvim-dap",
         dependencies = { "mfussenegger/nvim-dap-python" },
         config = function()
-        local dap_python = require("dap-python")
-        dap_python.setup(vim.fn.expand("$HOME/.cache/pypoetry/virtualenvs/nvim-*/bin/python"))
-
-        vim.api.nvim_set_keymap("n", "<F5>", ":lua require'dap'.continue()<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<F10>", ":lua require'dap'.step_over()<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<F11>", ":lua require'dap'.step_into()<CR>", { noremap = true, silent = true })
-        vim.api.nvim_set_keymap("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>", { noremap = true, silent = true })
+            require("dap-python").setup(
+                vim.fn.expand("$HOME/.cache/pypoetry/virtualenvs/nvim-*/bin/python")
+            )
+            map("n", "<F5>",      ":lua require'dap'.continue()<CR>")
+            map("n", "<F10>",     ":lua require'dap'.step_over()<CR>")
+            map("n", "<F11>",     ":lua require'dap'.step_into()<CR>")
+            map("n", "<leader>b", ":lua require'dap'.toggle_breakpoint()<CR>")
         end,
     },
-    })
+
+    -- =========================
+    -- LATEX
+    -- =========================
+    {
+        "lervag/vimtex",
+        config = function()
+            vim.g.vimtex_view_method = "zathura"
+        end,
+    },
+})
